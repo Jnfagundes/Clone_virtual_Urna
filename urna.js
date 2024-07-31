769e47442f0eedaf79f4e163d32ecd44d519ee1d
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2024-07-31 13:00:56 -0300

    Programando o botão

--- urna.js
+++ urna.js
@@ -5,8 +5,30 @@ const listaDeBotoes = document.querySelectorAll('button'); //seleciona todos os 
     listaDeBotoes.forEach((botao, index)=>{
         botao.addEventListener('click', () => {
             console.log(`vc clicou  no botão ${index + 1}`);
+
+            processarBotao(index + 1)
         });
     });
  }
 
+ function processarBotao(valor) {
+    // Estrutura condicional para verificar o valor
+    if (valor === 1) {
+        function criarElemento(){
+            const novaDiv = document.createElement('div');
+            novaDiv.innerHTML = `
+            <h2> criando aqui </>
+            `
+            const tela = document.getElementById('tela');
+            tela.appendChild(novaDiv);
+        };
+    } else if (valor === 2) {
+        console.log('Você clicou no Botão 2');
+    } else if (valor === 3) {
+        console.log('Você clicou no Botão 3');
+    } else {
+        console.log('Botão desconhecido');
+    }
+}
+ criarElemento();
  votando();
